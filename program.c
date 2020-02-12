dbb47c153d813608a1c68f4af4c9b10eb64d5ab7
ano12-dev <52732063+ano12-dev@users.noreply.github.com>
2020-02-12 16:17:20 +0530

    Add files via upload

--- program.c
+++ program.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 void add(int);
 void display();
 void add_beg(int);
@@ -15,23 +16,92 @@ struct node {
 
 int main()
 {
+	int usr_input = 1;
+	int _number, _pos, _old, _new, _sno, _del;
+	char boo;
 	first_node = NULL;
-      	add(30);
-	add_beg(20);
-	add(44);
-	add_beg(334);
-	insert(2,300);
-	display();
-	update(20,200);
-	printf(" After Update\n");
-	display();
-	printf(" Delete\n");
-	delete(334);
-	printf(" After delete\n");
-	display();
-	printf(" After reverse\n");
-	display();
-	return 0;
+	while(usr_input != 0)
+	{
+		printf(" Press 1 to Add: \n");
+		printf(" Press 2 to Add from beginning: \n");
+		printf(" Press 3 to Display items: \n");
+		printf(" Press 4 to insert in between: \n");
+		printf(" Press 5 to update item: \n");
+		printf(" Press 6 to search item: \n");
+		printf(" Press 7 to delete item: \n");
+		printf(" Press 8 to reverse the whole list: \n");
+		printf(" Press 0 to exit: \n");
+		printf(" Please enter your choice: ");
+		scanf("%d",&usr_input);
+		putchar('\n');
+		switch(usr_input)
+		{
+			case 1:
+				printf(" You have selected to add a number, please enter the number: ");
+				scanf("%d",&_number);
+				add(_number);
+				break;
+			case 2:
+				printf(" You have selected to add number from beginning, please enter the number: ");
+				scanf("%d",&_number);
+				add_beg(_number);
+				break;
+			case 3:
+				printf(" You have selected to display items: \n");
+				display();
+				break;
+			case 4:
+				printf(" You have selected to inster item in between: \n");
+				printf(" Please enter the position you wish to enter the number in: ");
+				scanf("%d",&_pos);
+				putchar('\n');
+				printf(" Please enter the number you wish to enter: ");
+				scanf("%d",&_number);
+				insert(_pos,_number);
+				break;
+			case 5:
+				printf(" You have selected to update the item in a list: \n");
+				printf(" Please enter the item you wish to update: \n");
+				printf(" Here are your items: \n");
+				display();
+				printf(" Enter the item from the above list to update: ");
+				scanf("%d",&_old);
+				printf(" Enter the number to replace the selected item: ");
+				scanf("%d",&_new);
+				update(_old,_new);
+				break;
+			case 6:
+				printf(" Enter the number you wish to search: \n");
+				scanf("%d",&_sno);
+				_pos = search(_sno);
+				if(_pos != -1)
+					printf(" Search number found at %d position.\n", _pos);
+				else
+					printf(" Search number not found.\n");
+				break;
+			case 7:
+				printf(" You have selected to delete an item.\n");
+				printf(" Here are your items. Select any one you wish to delete\n");
+				display();
+				printf(" Enter the item you wish to delete: ");
+				scanf("%d",&_number);
+				//printf(" Are you sure Y/N: ");
+				//scanf("%c",&boo);
+				//if(boo == 'Y' || boo == 'y')
+				delete(_number);
+				break;
+			case 8:
+				printf(" You have selected to reverse the whole list: \n");
+				reverse();
+				break;
+			case 0:
+				break;
+			default:
+				printf(" Please select the correct option.\n");
+				break;
+		}
+				
+	}
 }
 void add(int data)
 {
@@ -151,4 +221,18 @@ void delete(int data)
 	}
 	tmp2->next = tmp->next;
 	free(tmp);
+}
+void reverse()
+{
+	struct node *current, *pre, *next;
+	pre = first_node;
+	current = first_node->next;
+	pre->next = NULL;
+	while(current != NULL) {
+		next = current->next;
+		current->next = pre;
+		pre = current;
+		current = next;
+	}
+	first_node = pre;
 }
